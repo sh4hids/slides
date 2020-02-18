3a8da99d7fa21fcf8d3f1cfa54a82bee4de69d33
Shahidul Islam Majumder <dev@shahid.pro>
2020-02-18 15:49:26 +0600

    Add links slide

---
+++ src/views/components/Link.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+import { typography, color, space } from "styled-system"
+
+const Link = styled.a`
+  ${space};
+  ${typography};
+  ${color};
+
+  color: #74b816;
+`
+
+export default Link
